78d851a3278af4c74a90b5a3696b0ae393fbf062
Boldizsár Poór <boldar99@gmail.com>
2020-03-25 15:32:34 +0100

    Lesson 5 is done

--- Lesson5.hs
+++ Lesson5.hs
@@ -1,35 +1,59 @@
 module Lesson5 where
 
+import Prelude hiding 
+    (sum, last, init, minimum, concat, (++), merge, zip, isPrefixOf, elem, nub)
+
 sum :: Num a => [a] -> a
-sum = undefined
+sum []     = 0
+sum (x:xs) = x + sum xs
 
 last :: [a] -> a
-last = undefined
+last [x]    = x
+last (x:xs) = last xs
+-- Lehet errort dobni nem megfelelő esetben:
+last [] = error "empty list"
 
 init :: [a] -> [a]
-init = undefined
+init [x]    = []
+init (x:xs) = x : init xs
+-- Lehet errort dobni nem megfelelő esetben:
+init [] = error "empty list"
 
 minimum :: Ord a => [a] -> a
-minimum = undefined
+minimum [x]    = x
+minimum (x:xs) = x `min` minimum xs
+--               min x (minimum xs)
 
 concat :: [[a]] -> [a]
-concat = undefined
+concat []     = []
+concat (l:ls) = l ++ concat ls
+--              (++) l (concat ls)
 
 (++) :: [a] -> [a] -> [a]
-(++) = undefined
+[]     ++ ls = ls
+(x:xs) ++ ls = x : (xs ++ ls)
 
 merge :: [a] -> [a] -> [a]
-merge = undefined
+merge []     ys     = ys
+merge xs     []     = xs
+merge (x:xs) (y:ys) = x : y : merge xs ys
 
 zip :: [a] -> [b] -> [(a,b)]
 zip = undefined
 
 isPrefixOf :: Eq a => [a] -> [a] -> Bool
-isPrefixOf = undefined
+isPrefixOf [] [] = True
+isPrefixOf _  [] = False
+isPrefixOf [] _  = True
+isPrefixOf (x:xs) (y:ys)
+   | x == y = isPrefixOf xs ys
+   | x /= y = False
+-- | otherwise = ..
+-- | True = ..
 
 elem :: Eq a => a -> [a] -> Bool
-elem = undefined
-
--- Egyik házi lesz
-nub :: Eq a => [a] -> [a]
-nub = undefined
+elem _ [] = False
+elem a (x:xs)
+  | a == x    = True
+  | otherwise = a `elem` xs
+-- elem a (x:xs) = a == x || elem a xs
